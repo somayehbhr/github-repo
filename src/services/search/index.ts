ad20d70f3821ebe58594169827da90c48130dec9
somayeh.b <somayebhr@gmail.com>
2023-12-01 03:00:47 +0330

    feat: add search function using request.get

---
+++ src/services/search/index.ts
@@ -0,0 +1,8 @@
+import request from "../axios";
+
+export const search = async (endpoint: any, params: any) => {
+  const { data }: { data: any } = await request.get("/search/" + endpoint, {
+    params: { ...params },
+  });
+  return data;
+};
