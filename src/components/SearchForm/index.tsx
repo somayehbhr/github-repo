b8ec624a85700c8482b94516007e9ff53e500f3b
somayeh.b <somayebhr@gmail.com>
2023-12-02 12:27:33 +0330

    chore: change button text

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -35,7 +35,7 @@ export const SearchForm: FC<SearchFormProps> = ({ handleSearch }) => {
           type="submit"
           className="w-1/4 bg-blue-500 text-white px-4 py-2 rounded-r focus:outline-none hover:bg-blue-700"
         >
-          Submit
+          Search
         </button>
       </form>
     </div>
